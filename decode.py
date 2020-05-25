2974d92252a4ca6723220167a9395fa00f64639f
Paul Saikko <paul.saikko@gmail.com>
2020-05-25 19:51:13 +0300

    Add main

--- decode.py
+++ decode.py
@@ -7,25 +7,6 @@ import sys
 def helptext():
     print(f"Usage: `python3 {sys.argv[0]} <path to cmRally.cfg>`")
 
-if not len(sys.argv) == 2:
-    helptext()
-    exit(1)
-
-if not path.exists(sys.argv[1]):
-    helptext()
-    exit(1)
-
-with open(sys.argv[1], "rb") as savefile:
-    data = savefile.read()
-
-if not len(data) == 7712:
-    helptext()
-    exit(1)
-
-stage_records_chunk = data[4092:6162]
-stage_record_size = 30
-n_stage_records = len(stage_records_chunk) // stage_record_size
-
 def parse_name(byte_values):
     name = ""
     for val in byte_values:
@@ -36,7 +17,6 @@ def parse_name(byte_values):
 def parse_meta(data):
     car_id = data & 0x000F
     data >>= 4
-    car_id = constants.CMR_CAR_NAMES[car_id]
     is_human = data & 0x1
     data >>= 1
     is_manual = data & 0x1
@@ -50,53 +30,102 @@ def format_time(hundredths):
     hundredths = hundredths - seconds*100
     return f"{minutes:02d}:{seconds:02d}.{hundredths:02d}"
     
-print("STAGE TIMES "+'-'*114)
-for (i, stage_name) in enumerate(constants.CMR_STAGE_NAMES):
-    # Some fields are left blank in the save file
-    if not stage_name: continue
-    rally_name = constants.CMR_RALLY_NAMES[i // 7]
+def read_stage_times(savefile_data):
+    stage_records_chunk = savefile_data[4092:6162]
+    stage_record_size = 30
+    n_stage_records = len(stage_records_chunk) // stage_record_size
+    stage_data = []
+    for (i, stage_name) in enumerate(constants.CMR_STAGE_NAMES):
+        # Some fields are left blank in the save file
+        if not stage_name: continue
+        rally_name = constants.CMR_RALLY_NAMES[i // 7]
 
-    byte_values = stage_records_chunk[stage_record_size*i : stage_record_size*(i+1)]
-    fields = struct.unpack("<12sHHHHHHHHH", byte_values)
-    name = parse_name(fields[0])
-    split_times = fields[1:9]
-    metadata = fields[9]
+        byte_values = stage_records_chunk[stage_record_size*i : stage_record_size*(i+1)]
+        fields = struct.unpack("<12sHHHHHHHHH", byte_values)
+        player_name = parse_name(fields[0])
+        split_times = fields[1:9]
+        metadata = fields[9]
 
-    # Special stages have no checkpoint times
-    if i in constants.CMR_SPECIAL_STAGES:
-        split_times = split_times[-1:]
+        # Special stages have no checkpoint times
+        if i in constants.CMR_SPECIAL_STAGES:
+            split_times = split_times[-1:]
 
-    time_strings = map(format_time, split_times)
+        (car_id, is_manual, is_human) = parse_meta(metadata)
 
-    (car_id, is_manual, is_human) = parse_meta(metadata)
+        stage_data.append({
+            'Rally': rally_name,
+            'Stage': stage_name,
+            'Player': player_name,
+            'Times': split_times,
+            'Car': constants.CMR_CAR_NAMES[car_id],
+            'Manual': is_manual,
+            'Human': is_human
+        })
+    return stage_data
 
-    print('%13s'%rally_name, 
-        '%14s'%stage_name, 
-        '%12s'%name, 
-        '%71s'%' '.join(list(time_strings)),
-        car_id,
-        "M" if is_manual else "A")
+def read_rally_times(savefile_data):
+    rally_chunk = savefile_data[3452:4092]
+    rally_chunk_size = 16
+    n_rally_records = len(rally_chunk) // rally_chunk_size
+    rally_times = []
+    for i in range(n_rally_records):
+        rally_name = constants.CMR_RALLY_NAMES[i // 5]
+        rank = (i % 5) + 1
 
-rally_chunk = data[3452:4092]
-rally_chunk_size = 16
-n_rally_records = len(rally_chunk) // rally_chunk_size
+        byte_values = rally_chunk[rally_chunk_size*i : rally_chunk_size*(i+1)]
 
-print("RALLY TIMES "+'-'*114)
-for i in range(n_rally_records):
-    rally_name = constants.CMR_RALLY_NAMES[i // 5]
-    position = ["1st","2nd","3rd","4th","5th"][i % 5]
+        player_name, metadata = struct.unpack("<12sI", byte_values)
+        player_name = parse_name(player_name)
+        time = metadata & 0x0007FFFF
+        metadata >>= 19
+        (car_id, is_manual, is_human) = parse_meta(metadata)
 
-    byte_values = rally_chunk[rally_chunk_size*i : rally_chunk_size*(i+1)]
+        rally_times.append({
+            "Rally": rally_name,
+            "Rank": rank,
+            "Player": player_name,
+            "Time": time,
+            "Car": constants.CMR_CAR_NAMES[car_id],
+            "Manual": is_manual,
+            "Human": is_human
+        })
+    return rally_times
+        
 
-    name, metadata = struct.unpack("<12sI", byte_values)
-    name = parse_name(name)
-    time = metadata & 0x0007FFFF
-    metadata >>= 19
-    (car_id, is_manual, is_human) = parse_meta(metadata)
+if __name__ == "__main__":
+    if not len(sys.argv) == 2:
+        helptext()
+        exit(1)
 
-    print('%13s'%rally_name,
-        position,
-        '%12s'%name, 
-        format_time(time),
-        car_id,
-        "M" if is_manual else "A")
+    if not path.exists(sys.argv[1]):
+        helptext()
+        exit(1)
+
+    with open(sys.argv[1], "rb") as savefile:
+        data = savefile.read()
+
+    if not len(data) == 7712:
+        helptext()
+        exit(1)
+
+    print("-- STAGE TIMES --")
+    for record in read_stage_times(data):
+        print('%13s' % record["Rally"], 
+              '%14s' % record["Stage"], 
+              '%12s' % record["Player"], 
+              '%71s' % ' '.join(map(format_time, record["Times"])),
+              record["Car"],
+              "M" if record["Manual"] else "A")
+
+    print("\n-- RALLY TIMES --")
+    for record in read_rally_times(data):
+        print('%13s' % record["Rally"],
+              record["Rank"],
+              '%12s' % record["Player"], 
+              format_time(record["Time"]),
+              record["Car"],
+              "M" if record["Manual"] else "A")
+
+    
+
+    
