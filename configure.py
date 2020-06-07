fba38c6c4f3cf193e1d0671ffef320b0bdfcad62
Paul Saikko <paul.saikko@gmail.com>
2020-06-07 13:13:32 +0300

    add cmr2 path to config

--- configure.py
+++ configure.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-game_path = input("Game path: ")
+cmr_path = input("CMR path: ")
+cmr2_path = input("CMR2 path: ")
 api_url = input("API URL: ")
 api_key = input("API Key: ")
 
@@ -8,5 +9,6 @@ with open(".env", "w") as f:
     print(f"""\
 LEADERBOARD_API_URL="{api_url}"
 LEADERBOARD_API_KEY="{api_key}"
-CMR_INSTALL_PATH="{game_path}"
+CMR_INSTALL_PATH="{cmr_path}"
+CMR2_INSTALL_PATH="{cmr2_path}"
 """, end='', file=f)
